ac97bc493f82550eb975b967f1431b51bcbbb1f4
Pier Bruno Pompilii <pier340@gmail.com>
2023-12-13 12:17:45 +0000

    Add CI config3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello world!, my name is Pier Bruno"
+    return "Hello world!, my name is Pier Bruno and im good"
 
 if __name__ == '__main__':
     app.run(
